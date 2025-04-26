6928c8509a8575f86138e587fd845b8a217cc75b
Johan Walles <johan.walles@gmail.com>
2025-04-26 08:33:19 +0200

    Improve Python code readability

--- add_on/python_io.py
+++ add_on/python_io.py
@@ -49,6 +49,9 @@ def convert_to_python(node_system: NodeSystem) -> str:
     Returns:
         str: A string containing the Python script representation of the node system.
     """
+
+    indent = "    "
+
     # Start with the imports
     python_code = "from nodecode.shading import *\n\n"
 
@@ -58,19 +61,21 @@ def convert_to_python(node_system: NodeSystem) -> str:
     # Iterate over nodes in the node system
     for node in node_system.nodes:
         # Generate the constructor for the node
-        constructor = f"    {pythonify(node.name)} = {pythonify(node.type)}("
+        constructor = (
+            f"{indent}{pythonify(node.name)} = {pythonify(node.type)}(\n{indent * 2}"
+        )
 
         # Add input sockets as arguments
         inputs = []
         for input_socket in node.input_sockets:
             inputs.append(render_input_socket(input_socket))
-        constructor += ", ".join(inputs) + ")\n"
+        constructor += f",\n{indent * 2}".join(inputs) + ")\n"
         python_code += constructor
 
     # Add the return statement for the output node
     output_node = node_system.get_output_node()
     if output_node:
-        output_constructor = f"    return {pythonify(output_node.name)}("
+        output_constructor = f"{indent}return {pythonify(output_node.name)}("
         outputs = []
         for output_socket in output_node.output_sockets:
             outputs.append(
